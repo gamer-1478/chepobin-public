2285f3748a7d88be943e9391fec7a04b20122eaa
gamer-1478 <aayushgarg2006@gmail.com>
2021-10-04 11:35:04 +0530

    update

--- server.js
+++ server.js
@@ -215,5 +215,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
-app.listen(port, () =>
-    console.log(`Server is running on port ${port} on localhost`));
+app.listen(port, () => {
+    console.log(`Server is running on port ${port} on localhost`)
+});
